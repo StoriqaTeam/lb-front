2d1177d88bf0f591758afb68b3a7d4804083ed9f
mishanya <the.one.mishanya@gmail.com>
2018-07-17 13:58:21 +0300

    Added https

--- src/constants/constantsAPI.js
+++ src/constants/constantsAPI.js
@@ -1,8 +1,8 @@
 
 
 const API_URL = ({
-		'lb-front.stq.cloud': 'http://lb-back.stq.cloud:3022',
-		'localhost':      'http://localhost:5000'
+		'lb-front.stq.cloud': 'https://lb-back.stq.cloud:3022',
+		'localhost':      		'http://localhost:5000'
 	})[window.location.hostname]
 
 export  {API_URL}

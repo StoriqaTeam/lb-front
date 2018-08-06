6b374cd7ef4b0b97d80e660584fb6e96078446ab
Aleksandr Volkov <avolkov05@gmail.com>
2018-08-06 08:14:11 +0700

    fix url

--- src/components/elements/login_block/SocialLogin.js
+++ src/components/elements/login_block/SocialLogin.js
@@ -97,7 +97,7 @@ class SocialLogin extends Component {
 										<TwitterLogin loginUrl="https://api.twitter.com/oauth/request_token"
 										              onFailure={this.onFailed}
 										              onSuccess={this.onSuccess}
-										              requestTokenUrl={`${API_URL}/auth/twitter`}
+										              requestTokenUrl={`${API_URL}/api/v1/auth/twitter`}
 										              showIcon={true}
 										              customHeaders={'customHeader'}>
 											<svg fill="#fff" preserveAspectRatio="xMidYMid meet" height={25} width={25} viewBox="0 0 40 40" style={{verticalAlign: 'middle', color: 'rgb(0, 172, 237)'}}><g><path d="m37.7 9.1q-1.5 2.2-3.7 3.7 0.1 0.3 0.1 1 0 2.9-0.9 5.8t-2.6 5.5-4.1 4.7-5.7 3.3-7.2 1.2q-6.1 0-11.1-3.3 0.8 0.1 1.7 0.1 5 0 9-3-2.4-0.1-4.2-1.5t-2.6-3.5q0.8 0.1 1.4 0.1 1 0 1.9-0.3-2.5-0.5-4.1-2.5t-1.7-4.6v0q1.5 0.8 3.3 0.9-1.5-1-2.4-2.6t-0.8-3.4q0-2 0.9-3.7 2.7 3.4 6.6 5.4t8.3 2.2q-0.2-0.9-0.2-1.7 0-3 2.1-5.1t5.1-2.1q3.2 0 5.3 2.3 2.4-0.5 4.6-1.7-0.8 2.5-3.2 3.9 2.1-0.2 4.2-1.1z" /></g></svg>
